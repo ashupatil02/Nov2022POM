d1b8861fd8168834bfab8ce17a318701d639765e
Ashwini Patil <ashupatil02@gmail.com>
2023-03-14 12:27:24 -0400

    mail Ashwini

---
+++ src/main/java/com/qa/opencart/pages/Mail1.java
@@ -0,0 +1,19 @@
+package com.qa.opencart.pages;
+
+public class Mail1 {
+	
+		public void sendMail(){
+	    System.out.println("mail sent-----Ashwini");
+	    }
+		public void getMail() {
+			System.out.println("get mail---Ashwini");
+		}
+		public void close() {
+			System.out.println("close mail---Ashwini");
+		}
+
+
+	}
+
+
+
