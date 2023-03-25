89b4159262e2c5a54cb05a4f37249b86a947f8a2
Ashwini Patil <ashupatil02@gmail.com>
2023-03-25 02:21:13 -0400

    updated Rp

--- src/main/java/com/qa/opencart/pages/ViewCartPopUpPage.java
+++ src/main/java/com/qa/opencart/pages/ViewCartPopUpPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 public class ViewCartPopUpPage {
 
 	public ViewCartPopUpPage(WebDriver driver) {
-		// TODO Auto-generated constructor stub
+		System.out.println("");
 	}
 
 	
